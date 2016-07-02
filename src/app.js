c1de369f9d13f04051170961a8d23500383ef0ee
Freddie FUJIWARA <github@ze.gs>
2016-07-02 20:45:44 +0900

    Version 0.1

--- src/app.js
+++ src/app.js
@@ -3,86 +3,55 @@
  *
  * This is where you write your app.
  */
-
 var UI = require('ui');
 var Vector2 = require('vector2');
+var Feature = require('platform/feature');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
+
+var wind = new UI.Window({
+  backgroundColor: 'white'
+});
+var cupolan = [];
+var x = Feature.resolution().x;
+var y = Feature.resolution().y;
+for(var i = 0; i < 10; i++){
+  cupolan.push(new UI.Image({
+    position: new Vector2(0, (y-x)/4),
+    size: new Vector2(x,x),
+    backgroundColor: 'white',
+    image: 'images/cupolan-pose'+i+'.png'
+  }));
+}
+
+var timeText = new UI.TimeText({
+  position: new Vector2(0, y-76),
+  size: new Vector2(x,74),
+  text: "%m-%d\n%X",
+  font: 'leco-28-light-numbers',
+  color: 'vividCerulean',
+  textAlign: 'center',
+  backgroundColor: 'white',
+  textOverflow:'wrap'
 });
 
-main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }, {
-        title: 'Third Item',
-      }, {
-        title: 'Fourth Item',
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    backgroundColor: 'black'
-  });
-  var radial = new UI.Radial({
-    size: new Vector2(140, 140),
-    angle: 0,
-    angle2: 300,
-    radius: 20,
-    backgroundColor: 'cyan',
-    borderColor: 'celeste',
-    borderWidth: 1,
-  });
-  var textfield = new UI.Text({
-    size: new Vector2(140, 60),
-    font: 'gothic-24-bold',
-    text: 'Dynamic\nWindow',
-    textAlign: 'center'
-  });
-  var windSize = wind.size();
-  // Center the radial in the window
-  var radialPos = radial.position()
-      .addSelf(windSize)
-      .subSelf(radial.size())
-      .multiplyScalar(0.5);
-  radial.position(radialPos);
-  // Center the textfield in the window
-  var textfieldPos = textfield.position()
-      .addSelf(windSize)
-      .subSelf(textfield.size())
-      .multiplyScalar(0.5);
-  textfield.position(textfieldPos);
-  wind.add(radial);
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+var currentCupolan = null;
+var currentMin = null;
+var clockThread = function(){
+    var now = new Date();
+    var min = now.getMinutes() ;
+    if( currentMin !== min ){
+      if( null !== currentCupolan ){
+        wind.remove(currentCupolan);
+      }
+      if( null !== timeText ){
+        wind.remove(timeText);
+      }
+      currentCupolan = cupolan[min % 10];
+  
+      wind.add(currentCupolan);
+      wind.add(timeText);
+      currentMin = min;
+    }
+};
+setInterval(clockThread,1000);
+wind.show();
